f861aa28e82891c359980dc08c6f81248db8ff78
Ivan Kalinić <ivankalinic1998@gmail.com>
2022-02-14 10:34:08 +0100

    Removed homepage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,5 @@
-import { Flex } from "@chakra-ui/react";
 import { lazy, Suspense } from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Background } from "../assets/svg";
 import { Loader } from "../components/Loader";
 import { useUser } from "../context/UserContext";
@@ -13,7 +7,6 @@ import { Footer } from "../global_styles";
 
 const Error = lazy(() => import("../components/Error"));
 const Logout = lazy(() => import("../components/Logout"));
-const HomePage = lazy(() => import("../pages/HomePage"));
 const Login = lazy(() => import("../pages/Login"));
 const Movies = lazy(() => import("../pages/Movies"));
 const AddMovie = lazy(() => import("../pages/AddMovie"));
